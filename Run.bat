2fffd8b3af5b52c257df3720077a5c4cd90c174f
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-04-22 14:35:32 -0400

    Change scripts to use cake

--- Run.bat
+++ Run.bat
@@ -1,11 +1,2 @@
 @echo off
-rem move to the script directory
-cd /d "%~dp0"
-rem build the mod
-dotnet build
-rem copy the mod to the mods folder
-robocopy ./bin/Debug/net7.0/ "%LOCALAPPDATA%/Tiny Life/Mods" /e /is
-rem run the game
-set /p dir=<"%LOCALAPPDATA%/Tiny Life/GameDir"
-cd /d %dir%
-set HARMONY_DEBUG=1 && "Tiny Life.exe" -v
+dotnet cake --target=Run
--- Run.sh
+++ Run.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-# move to the script directory
-cd "$(dirname "$0")"
-# build the mod
-dotnet build
-# copy the mod to the mods folder
-cp ./bin/Debug/net7.0/* "$HOME/.local/share/Tiny Life/Mods" -r
-# run the game
-dir=$(<"$HOME/.local/share/Tiny Life/GameDir")
-cd $dir
-"./Tiny Life" -v
-
-# TODO: Replace this system with the cake build system. Shouldn't be difficult, but prioritizing working.
+dotnet cake --target=Run
