3a288432429020608b88570df081af435831005f
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-12-12 04:19:45 -0500

    Update to version 0.38.0 Add TestedVersionRange

--- IttyMod.cs
+++ IttyMod.cs
@@ -30,6 +30,8 @@ namespace IttyMod {
         public override string Name => "Itty";
         public override string Description => "Brings your Tiny Life online!\nThis might be a mistake...";
         public override string IssueTrackerUrl => "https://github.com/ssblur/IttyMod/issues";
+        public override string TestedVersionRange => "[0.38.0,0.38.0]";
+
         public override TextureRegion Icon => IttyMod.uiTextures[0, 0];
         public override bool RequiresHarmony => false;
 
