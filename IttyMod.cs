246e510c8e3a92fb966b7d8d9ef673238b3dda37
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-09-03 18:05:53 -0400

    Update to reflect compatibility with 0.43.10

--- IttyMod.cs
+++ IttyMod.cs
@@ -30,7 +30,7 @@ namespace IttyMod {
         public override string Name => "Itty";
         public override string Description => "Brings your Tiny Life online!\nThis might be a mistake...";
         public override string IssueTrackerUrl => "https://github.com/ssblur/IttyMod/issues";
-        public override string TestedVersionRange => "[0.43.6,0.43.6]";
+        public override string TestedVersionRange => "[0.43.6,0.43.10]";
 
         public override TextureRegion Icon => IttyMod.uiTextures[0, 0];
         public override bool RequiresHarmony => false;
